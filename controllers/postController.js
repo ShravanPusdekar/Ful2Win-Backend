bcd15c8eb5f8b9d80561c17f7b281a1cd577fb83
Swapnil Pawar <sp1880109@gmail.com>
2025-10-03 12:01:18 +0530

    follow Unfollow integrate

--- controllers/postController.js
+++ controllers/postController.js
@@ -781,7 +781,10 @@ const getPostsByUser = async (req, res) => {
     if (!userId) {
       return res.status(404).json({ message: 'User not found' });
     }
-    const posts = await Post.find({ 'author': userId });
+    const posts = await Post.find({ 'author': userId 
+    }).sort({ createdAt: -1
+    });
+    
     res.json(posts);
   } catch (error) {
     res.status(500).json({ message: error.message });
