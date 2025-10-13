16837b4f83a64598b7297d057010b3bae7edede5
Swapnil Pawar <sp1880109@gmail.com>
2025-10-13 16:57:49 +0530

    challenge work

--- routes/deviceRoute.js
+++ routes/deviceRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { registerDevice } from "../controllers/deviceController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
-router.post("/register-device", registerDevice);
+router.post("/register-device", protect, registerDevice);
 
 export default router;
